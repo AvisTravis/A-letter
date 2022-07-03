8c8ad6c46c847d93b5d97d4de1cd29a25c5219b8
AvisTravis <abhishekacharya261@gmail.com>
2022-07-03 13:22:08 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,315 @@
+const root = document.getElementById('root')
+
+let letter = "Hello ";
+let trail = "..."
+setTimeout(function() {
+    letter = letter 
+    root.innerHTML = letter + trail
+}, 500);
+
+setTimeout(function() {
+    letter = letter + " Sristika" 
+    root.innerHTML = letter + trail 
+}, 1000);
+
+setTimeout(function() {
+    letter = letter + ", " 
+    root.innerHTML = letter + trail 
+}, 1500);
+
+setTimeout(function() {
+    letter = letter + " I " 
+    root.innerHTML = letter + trail 
+}, 2000);
+
+setTimeout(function() {
+    letter = letter + " am " 
+    root.innerHTML = letter + trail 
+}, 2500);
+
+setTimeout(function() {
+    letter = letter + " writing " 
+    root.innerHTML = letter + trail 
+}, 3000);
+
+setTimeout(function() {
+    letter = letter + " you " 
+    root.innerHTML = letter + trail 
+}, 3500);
+
+setTimeout(function() {
+    letter = letter + " this " 
+    root.innerHTML = letter + trail 
+}, 4000);
+
+setTimeout(function() {
+    letter = letter + " letter " 
+    root.innerHTML = letter + trail 
+}, 4500);
+
+setTimeout(function() {
+    letter = letter + " because " 
+    root.innerHTML = letter + trail 
+}, 5000);
+
+setTimeout(function() {
+    letter = letter + " it " 
+    root.innerHTML = letter + trail 
+}, 5500);
+
+setTimeout(function() {
+    letter = letter + " had " 
+    root.innerHTML = letter + trail 
+}, 6000);
+
+setTimeout(function() {
+    letter = letter + " been " 
+    root.innerHTML = letter + trail 
+}, 6500);
+
+setTimeout(function() {
+    letter = letter + " a " 
+    root.innerHTML = letter + trail 
+}, 7000);
+
+setTimeout(function() {
+    letter = letter + " while " 
+    root.innerHTML = letter + trail 
+}, 7500);
+
+setTimeout(function() {
+    letter = letter + " that " 
+    root.innerHTML = letter + trail 
+}, 8000);
+
+setTimeout(function() {
+    letter = letter + " I've " 
+    root.innerHTML = letter + trail 
+}, 8500);
+
+setTimeout(function() {
+    letter = letter + " written " 
+    root.innerHTML = letter + trail 
+}, 9000);
+
+setTimeout(function() {
+    letter = letter + " you " 
+    root.innerHTML = letter + trail 
+}, 9500);
+
+setTimeout(function() {
+    letter = letter + " any " 
+    root.innerHTML = letter + trail 
+}, 10000);
+
+setTimeout(function() {
+    letter = letter + " letter " 
+    root.innerHTML = letter + trail 
+}, 10500);
+
+setTimeout(function() {
+    letter = letter + " at " 
+    root.innerHTML = letter + trail 
+}, 11000);
+
+setTimeout(function() {
+    letter = letter + " all" 
+    root.innerHTML = letter + trail 
+}, 11500);
+
+setTimeout(function() {
+    letter = letter + ". " 
+    root.innerHTML = letter + trail 
+}, 12000);
+
+setTimeout(function() {
+    letter = letter + " And" 
+    root.innerHTML = letter + trail 
+}, 12500);
+
+setTimeout(function() {
+    letter = letter + ", " 
+    root.innerHTML = letter + trail 
+}, 13000);
+
+setTimeout(function() {
+    letter = letter + " I " 
+    root.innerHTML = letter + trail 
+}, 13500);
+
+setTimeout(function() {
+    letter = letter + " thought " 
+    root.innerHTML = letter + trail 
+}, 14000);
+
+setTimeout(function() {
+    letter = letter + " I'd " 
+    root.innerHTML = letter + trail 
+}, 14500);
+
+setTimeout(function() {
+    letter = letter + " do " 
+    root.innerHTML = letter + trail 
+}, 15000);
+
+setTimeout(function() {
+    letter = letter + " it " 
+    root.innerHTML = letter + trail 
+}, 15500);
+
+setTimeout(function() {
+    letter = letter + " in " 
+    root.innerHTML = letter + trail 
+}, 15900);
+
+setTimeout(function() {
+    letter = letter + " a " 
+    root.innerHTML = letter + trail 
+}, 16200);
+
+setTimeout(function() {
+    letter = letter + " more " 
+    root.innerHTML = letter + trail 
+}, 16500);
+
+setTimeout(function() {
+    letter = letter + " fancy " 
+    root.innerHTML = letter + trail 
+}, 17000);
+
+setTimeout(function() {
+    letter = letter + " way" 
+    root.innerHTML = letter + trail 
+}, 17500);
+
+setTimeout(function() {
+    letter = letter + ". " 
+    root.innerHTML = letter + trail 
+}, 18000);
+
+setTimeout(function() {
+    letter = letter + " I " 
+    root.innerHTML = letter + trail 
+}, 18500);
+
+setTimeout(function() {
+    letter = letter + " guess " 
+    root.innerHTML = letter + trail 
+}, 19000);
+
+setTimeout(function() {
+    letter = letter + " I " 
+    root.innerHTML = letter + trail 
+}, 19500);
+
+setTimeout(function() {
+    letter = letter + " just " 
+    root.innerHTML = letter + trail 
+}, 20000);
+
+setTimeout(function() {
+    letter = letter + " want " 
+    root.innerHTML = letter + trail 
+}, 20500);
+
+setTimeout(function() {
+    letter = letter + " to " 
+    root.innerHTML = letter + trail 
+}, 21000);
+
+setTimeout(function() {
+    letter = letter + " let " 
+    root.innerHTML = letter + trail 
+}, 21500);
+
+setTimeout(function() {
+    letter = letter + " you " 
+    root.innerHTML = letter + trail 
+}, 22000);
+
+setTimeout(function() {
+    letter = letter + " know " 
+    root.innerHTML = letter + trail 
+}, 22500);
+
+setTimeout(function() {
+    letter = letter + " that " 
+    root.innerHTML = letter + trail 
+}, 23000);
+
+setTimeout(function() {
+    letter = letter + " you're " 
+    root.innerHTML = letter + trail 
+}, 23500);
+
+setTimeout(function() {
+    letter = letter + " appreciated" 
+    root.innerHTML = letter + trail 
+}, 24000);
+
+setTimeout(function() {
+    letter = letter + ". " 
+    root.innerHTML = letter + trail 
+}, 24500);
+
+setTimeout(function() {
+    letter = letter + " And" 
+    root.innerHTML = letter + trail 
+}, 25000);
+
+setTimeout(function() {
+    letter = letter + ", " 
+    root.innerHTML = letter + trail 
+}, 25500);
+
+setTimeout(function() {
+    letter = letter 
+    root.innerHTML = letter + ". . . "
+}, 26000);
+
+setTimeout(function() {
+    letter = letter 
+    root.innerHTML = letter + ". . . . . . "
+}, 26500);
+
+setTimeout(function() {
+    letter = letter 
+    root.innerHTML = letter + ". . . . . . . . . "
+}, 27000);
+
+setTimeout(function() {
+    letter = letter + " Ugh ...  " 
+    root.innerHTML = letter + trail 
+}, 27500);
+
+setTimeout(function() {
+    letter = letter + " Would " 
+    root.innerHTML = letter + trail 
+}, 28000);
+
+setTimeout(function() {
+    letter = letter + " you " 
+    root.innerHTML = letter + trail 
+}, 28500);
+
+setTimeout(function() {
+    letter = letter + " . . . " 
+    root.innerHTML = letter + "" 
+}, 31000);
+
+setTimeout(function() {
+    letter = letter + " <span> marry </span>" 
+    root.innerHTML = letter + trail 
+}, 35000);
+
+setTimeout(function() {
+    letter = letter + "<span> me? </span>" 
+    root.innerHTML = letter 
+}, 35250);
+
+
+setTimeout(function() {
+   
+    root.innerHTML = `${letter} <button onClick="location.href='https://www.youtube.com/watch?v=8pvHZ4ddR-4'"> Yes </button>` 
+}, 35500);
+
